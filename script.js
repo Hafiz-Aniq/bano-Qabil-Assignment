0c1a5d5c7c020b6d100b01b3af3fce8a6548bfc0
Hafiz-Aniq <hafizaniq1232004@gmail.com>
2024-01-18 12:16:41 +0500

    First Assignment

---
+++ script.js
@@ -0,0 +1,27 @@
+//Assignment Number (1) part (1)
+let age = 19;
+let isStudent = true;
+
+console.log("Age:", age);
+console.log("Is student?", isStudent);
+
+//Assignment Number (1) part (2)
+let greeting = "Welcome to Our Website!";
+let introduction =
+  "We are excited to have you here. Explore and enjoy our content.";
+
+alert(greeting + "\n" + introduction);
+
+//Assignment Number (2) part (1)
+let length = 5;
+let width = 8;
+
+let area = length * width;
+
+console.log("The area of the rectangle is:", area);
+
+//Assignment Number (2) part (2)
+
+let username = prompt("What is your name?");
+
+alert("Hello, " + username + "! Welcome to our website.");
